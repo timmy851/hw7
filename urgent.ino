f2825c946b6214b9998f87e25f25d0e56a692d4e
timmy851 <jmelizanis@gmail.com>
2018-03-05 23:12:29 -0800

    Update urgent.ino

--- urgent.ino
+++ urgent.ino
@@ -1,1 +1,22 @@
+// the setup function runs once when you press reset or power the board
+int ledPin1 = 9;   
+int ledPin2 = 5; 
+int ledPin3 = 6; 
 
+void setup() {
+  // initialize digital pin 13 as an output.
+  
+}
+ 
+// the loop function runs over and over again forever
+
+void loop() {
+  digitalWrite(ledPin1, HIGH);   // sets the LED on
+  delay(10);                  // waits for a second
+  digitalWrite(ledPin2, HIGH);    // sets the LED off
+  delay(10);  
+  digitalWrite(ledPin1, LOW);   // sets the LED on
+  delay(10);                  // waits for a second
+  digitalWrite(ledPin2, LOW);    // sets the LED off
+  delay(10); 
+}
